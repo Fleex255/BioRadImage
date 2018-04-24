a3612077f40fd382ecdec626fbeea932c83d1db2
Ben Nordick <Fleex255@users.noreply.github.com>
2018-04-24 11:18:59 -0500

    Update assembly information

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("BioRadImage")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("Ben Nordick")]
 [assembly: AssemblyProduct("BioRadImage")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
